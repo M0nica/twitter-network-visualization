614302867a8ce5e4ee6509eab77058cfb4c26492
Monica Powell <mmp2181@barnard.edu>
2016-03-03 00:04:38 -0500

    color-coded the networks

---
+++ trump.js
@@ -0,0 +1,95 @@
+<script>
+// get the data
+d3.csv("force_trump.csv", function(error, links) {
+var nodes = {};
+// Compute the distinct nodes from the links.
+links.forEach(function(link) {
+    link.source = nodes[link.source] ||
+        (nodes[link.source] = {name: link.source});
+    link.target = nodes[link.target] ||
+        (nodes[link.target] = {name: link.target});
+    link.value = +link.value;
+});
+var width = 2000,
+    height = 2000;
+  // var width = window.innerWidth,
+   // height = window.innerHeight; 
+
+var force = d3.layout.force()
+    .nodes(d3.values(nodes))
+    .links(links)
+    .size([width, height])
+    .linkDistance(60)
+    .charge(-300)
+    .on("tick", tick)
+    .start();
+
+    d3.select("button")
+                .on("click", function() {
+
+force.stop();
+
+d3.selectAll("g.nodes").transition().duration(500).attr("translate","transform("+newLayoutX+","+newLayoutY+")")
+
+});
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+// build the arrow.
+svg.append("svg:defs").selectAll("marker")
+    .data(["end"])      // Different link/path types can be defined here
+  .enter().append("svg:marker")    // This section adds in the arrows
+    .attr("id", String)
+    .attr("viewBox", "0 -5 10 10")
+    .attr("refX", 15)
+    .attr("refY", -1.5)
+    .attr("markerWidth", 6)
+    .attr("markerHeight", 6)
+    .attr("orient", "auto")
+  .append("svg:path")
+    .style("fill", function(d) { return "red"; })
+    .attr("d", "M0,-5L10,0L0,5");
+
+// add the links and the arrows
+var path = svg.append("svg:g").selectAll("path")
+    .data(force.links())
+  .enter().append("svg:path")
+//    .attr("class", function(d) { return "link " + d.type; })
+    .attr("class", "link")
+    .attr("marker-end", "url(#end)");
+// define the nodes
+var node = svg.selectAll(".node")
+    .data(force.nodes())
+  .enter().append("g")
+    .attr("class", "node")
+    .call(force.drag);
+// add the nodes
+node.append("circle")
+    .attr("r", 5);
+// add the text
+node.append("text")
+    .attr("x", 12)
+    .attr("dy", ".35em")
+    .text(function(d) { return d.name; });
+
+// add the curvy lines
+function tick() {
+    path.attr("d", function(d) {
+      //trasnformation - curve between links
+        var dx = d.target.x - d.source.x,
+            dy = d.target.y - d.source.y,
+            dr = Math.sqrt(dx * dx + dy * dy);
+        return "M" +
+            d.source.x + "," +
+            d.source.y + "A" +
+            dr + "," + dr + " 0 0,1 " +
+            d.target.x + "," +
+            d.target.y;
+    });
+    node
+        .attr("transform", function(d) {
+  	    return "translate(" + d.x + "," + d.y + ")"; });
+}
+
+});
+</script>
